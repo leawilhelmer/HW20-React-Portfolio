11590641b7e1086704f84cdc87662b4ea689abce
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-02 20:20:55 -0800

    updated components folders

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Footer/style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Lea Wilhelmer 2021</span>
+    </footer>
+  );
+}
+
+export default Footer;
