ae69a848fccbcb67f976e4afb9f26204f750a90c
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-02 20:45:54 -0800

    installed bootstrap and router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/About/aboutMe";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
+import Projects from "./components/Projects/projects"
+import Contact from "./components/Contact/contact"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/navbar" component={Navbar} />
+          <Route exact path="/footer" component={Footer} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,3 +1,2 @@
 import React from "react";
 import "../Projects/style.css";
-
