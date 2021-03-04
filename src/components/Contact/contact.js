c41d7e73fcfab0c03701d064481d94438849541d
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-04 12:26:40 -0800

    updated Contact.js

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "../Contact/style.css";
+import Row from "../Row/row";
+import Column from "../Column/column";
+import Resume from "../Resume/Lea's Resume.pdf"
 
 const Contact = () => {
-    const history = useHistory()
-	const handleClick = () => {
-		history.push('/Applications')
-	}
 
     return (
 				<Row>
@@ -16,11 +15,43 @@ const Contact = () => {
 					</Column>
 					<Row>
 						<Column size="md-8">
-							<p style={styles.Contact}>
+							<p>
                             Phone Number: +1 (408) 596-6644
                             Email:<a href="https://mail.google.com/mail/?view=cm&fs=1&to=leawilhelmer@gmail.com">leawilhelmer@gmail.com</a>
-
 							</p>
+
+							<Row>
+							<Column size="md-4">
+								<a
+									href={Resume}
+									download=""
+									id="updateResume"
+								>
+									<i className="fas fa-file-download"></i>
+									<br></br>
+									<span>Resume</span>{" "}
+								</a>
+								<br></br>
+							</Column>
+							<Column size="md-4">
+								<a href="https://www.linkedin.com/in/lea-wilhelmer-0101">
+									<i className="fab fa-linkedin">
+										{" "}
+									</i>
+									<br></br>
+									<span> LinkedIn</span>
+								</a>
+								<br></br>
+							</Column>
+							<Column size="md-4">
+								<a href="https://github.com/leawilhelmer" class="links">
+									<i className="fab fa-github-square"></i>
+									<br></br>
+									<span> GitHub</span>
+								</a>
+							</Column>
+						</Row>
+
 						</Column>
 					</Row>
 				</Row>
@@ -28,4 +59,4 @@ const Contact = () => {
     )
 }
 
-export default AboutMe
+export default Contact
