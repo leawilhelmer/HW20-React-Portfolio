9f3d8e88b089f81c03ef9db2760fe992c9e187d5
Lea Wilhelmer <leawilhelmer@gmail.com>
2021-03-04 18:56:14 -0800

    finished navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,37 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../Navbar/style.css";
 
-// const Navbar = () => (
-//   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <Link href="/">
-//       <a className="navbar-brand">LEA WILHELMER</a>
-//     </Link>
-//     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon" />
-//     </button>
-
-//     <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-//       <ul className="navbar-nav">
-//         <li className="nav-item">
-//           <Link href="/">
-//             <a className="nav-link">PROJECTS</a>
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link href="/about">
-//             <a className="nav-link">ABOUT</a>
-//           </Link>
-//         </li>
-//         <li className="nav-item">
-//           <Link href="/contact">
-//             <a className="nav-link">CONTACT</a>
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   </nav>
-// );
-
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
